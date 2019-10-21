5745fb5a96d246d95a5312c43a07d1715f587f16
tubins <tubin.shaji@gmail.com>
2019-10-21 18:37:23 +0100

    Add Spring boot project setup

---
+++ RandomNumberService/src/main/java/com/immense/RandomNumberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.immense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNumberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomNumberServiceApplication.class, args);
+	}
+
+}
---
+++ RandomNumberService/src/test/java/com/immense/RandomNumberServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.immense;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RandomNumberServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
